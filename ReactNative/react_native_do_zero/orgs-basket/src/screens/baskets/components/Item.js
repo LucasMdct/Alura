6388f7af9cb4514fa6012b63903bce1195162ab7
LucasMdct <lucasmedeiroscosta07@gmail.com>
2023-08-22 16:53:55 -0300

    FLAT LIST

---
+++ ReactNative/react_native_do_zero/orgs-basket/src/screens/baskets/components/Item.js
@@ -0,0 +1,32 @@
+import React from "react";
+import MyText from "../../../components/Text";
+import { View, Image, StyleSheet } from "react-native";
+
+
+export default function Item({ item: { name, img } }) {
+    return <View style={StyleItems.liststyle}>
+            <Image source={img} style={StyleItems.imge}></Image>
+            <MyText style={StyleItems.name}>{name}</MyText>
+        </View>
+}
+
+const StyleItems = StyleSheet.create({
+    liststyle: {
+        flexDirection: "row",
+        borderBottomWidth: 1,
+        borderBottomColor: "#ececec",
+        padding: 16,
+        marginHorizontal: 16,
+        alignItems: "center",
+    },
+    imge: {
+        width: 46,
+        height: 46,
+    },
+    name: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 11,
+        color: "#363636",
+    },
+});
--- ReactNative/react_native_do_zero/orgs-basket/src/screens/baskets/index.js
+++ ReactNative/react_native_do_zero/orgs-basket/src/screens/baskets/index.js
@@ -1,26 +1,40 @@
 import React from "react";
-import {StyleSheet, View, ScrollView} from "react-native";
-
+import { StyleSheet, View, FlatList } from "react-native";
 import Top from "./components/Top";
 import DetailsBasket from "./components/DetailsBasket";
-import Items from "./components/Items";
+import Item from "./components/Item";
+import MyText from "../../components/Text";
 
 
-function Basket({top , details, BuyItems}) {
-    return <ScrollView>
-        <Top {...top}/>
-        <View style={stylebasket.basket}>
-        <DetailsBasket {...details}/>
-        <Items {...BuyItems}/>
-        </View>
-    </ScrollView>
+export default function Basket({ top, details, BuyItems }) {
+    return <>
+        <FlatList
+            data={BuyItems.list}
+            renderItem={Item}
+            keyExtractor={({ name }) => name }
+            ListHeaderComponent={() => {
+                return <>
+                    <Top {...top} />
+                    <View style={stylebasket.basket}>
+                        <DetailsBasket {...details} />
+                        <MyText style={stylebasket.title}>{BuyItems.title}</MyText>
+                    </View>
+                </>
+            }} />
+            </>
 }
 
 const stylebasket = StyleSheet.create({
+    title: {
+        color: "#464646",
+        fontWeight: "bold",
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 20,
+        lineHeight: 32,
+    },
     basket: {
         paddingVertical: 8,
         paddingHorizontal: 16,
     },
 });
-
-export default Basket;
