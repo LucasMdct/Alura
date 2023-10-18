733a76ef4bd793c071964c382b5a70626e9006a1
LucasMdct <lucasmedeiroscosta07@gmail.com>
2023-10-18 08:34:33 -0300

    OnClick Colaborador test

--- React/organomain/src/componentes/Colaborador/index.js
+++ React/organomain/src/componentes/Colaborador/index.js
@@ -3,7 +3,11 @@ import { AiFillCloseCircle } from 'react-icons/ai';
 
 const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
-        <AiFillCloseCircle size={25} className="deletar" onClick={aoDeletar} />
+        <AiFillCloseCircle 
+            size={25}  
+            className="deletar" 
+            onClick={aoDeletar} 
+        />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
--- React/organomain/src/componentes/Time/index.js
+++ React/organomain/src/componentes/Time/index.js
@@ -1,13 +1,15 @@
 import Colaborador from '../Colaborador'
 import './time.css'
+import hexToRgba from 'hex-to-rgba';
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
 
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
+            <input value={time.corSecundaria} type='color' className='input-color' onChange={evento => mudarCor(evento.target.value, time.id)} />
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar} />)}
+                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar} />)}
             </div>
         </section>
 
