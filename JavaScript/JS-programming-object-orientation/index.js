6f141ef12f04b614b250be6c84efadc7bfbd7248
LucasMdct <lucasmedeiroscosta07@gmail.com>
2024-02-08 10:39:52 -0300

    New Course - JavaScript Programming Object Orientation

---
+++ JavaScript/JS-programming-object-orientation/index.js
@@ -0,0 +1,15 @@
+const customer1Name = "Lucas";
+const customer1Cpf = 11122244409;
+const customer1Agency = 1001;
+const customer1Balance = 0;
+
+const customer2Name = "Douglas";
+const customer2Cpf = 11166644409;
+const customer2Agency = 1001;
+const customer2Balance = 7005456;
+
+const customer3Name = "Cleber";
+const customer3Cpf = 11166655509;
+const customer3Agency = 1001;
+const customer3Balance = 250000;
+
